b361c021c7e197e657a0a9220245160103b8dc2f
Matt Thorning <matthewthorning@gmail.com>
2020-08-14 08:30:21 +0100

    changed width

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,10 @@
 <style>
-  div.photo {
-    width: 100%;
+  div {
     max-width: 56em;
+    margin: auto;
+  }
+  .photo {
+    width: 100%;
     border: 1px solid #f0f0f0;
     height: 300px;
     margin: 20px auto;
@@ -19,19 +22,21 @@
   <title>About</title>
 </svelte:head>
 
-<h1>Hi, I'm Matt.</h1>
-<div class="photo" />
+<div>
+  <h1>Hi, I'm Matt.</h1>
+  <div class="photo" />
 
-<p>
-  I'm a software developer who also enjoys taking photos; this site is for
-  showing off some of my favourite shots. Perhaps one day I will sell prints
-  from here, perhaps one day there will still only be a couple of mediocre
-  images because I have magpied my way onto the next shiny thing that takes my
-  interest!
-</p>
+  <p>
+    I'm a software developer who also enjoys taking photos; this site is for
+    showing off some of my favourite shots. Perhaps one day I will sell prints
+    from here, perhaps one day there will still only be a couple of mediocre
+    images because I have magpied my way onto the next shiny thing that takes my
+    interest!
+  </p>
 
-<p>
-  If you want to get in touch for any reason then please contact me on
-  <a href="mailto:photos@matt-thorning.dev">photos@matt-thorning.dev</a>
-  or through the social media links above. :)
-</p>
+  <p>
+    If you want to get in touch for any reason then please contact me on
+    <a href="mailto:photos@matt-thorning.dev">photos@matt-thorning.dev</a>
+    or through the social media links above. :)
+  </p>
+</div>
--- src/routes/image/_layout.svelte
+++ src/routes/image/_layout.svelte
@@ -1,20 +1,14 @@
 <script>
-  import Nav from '../components/Nav.svelte'
-
   export let segment
 </script>
 
 <style>
   main {
-    min-height: calc(100vh - 57px);
-    max-width: 137em;
-    padding: 2em;
+    max-width: 56em;
     margin: 0 auto;
     box-sizing: border-box;
   }
 </style>
-
-<Nav {segment} />
 
 <main>
   <slot />
