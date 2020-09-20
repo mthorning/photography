05d06864d6c0f633e22df666d70d335a0e1cf2cc
Matt Thorning <matthewthorning@gmail.com>
2020-09-20 17:58:29 +0100

    add env for node side

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ import polka from 'polka';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import env from 'node-env-file'
+
+env(__dirname + '/.env');
 
 const {PORT, NODE_ENV} = process.env;
 const dev = NODE_ENV === 'development';
+
 
 polka() // You can also use Express
     .use(
